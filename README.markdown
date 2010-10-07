ad5aeb390d4039a66c2dad008554939d529777a9
Hatem Mahmoud <hatem@expressionlab.com>
2010-10-07 11:23:45 +0200

    Initial commit

---
+++ README.markdown
@@ -0,0 +1,3 @@
+# Tasky
+
+Tasky is the best task manager for [Getting Things Done](http://en.wikipedia.org/wiki/Getting_Things_Done).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasky::Application.config.secret_token = 'f3d24728104739d4f47cd13450488461b7c5962aaf758069d2e7b69503557935e202a373e5d6d19eaac0de38458f2b0a786eac10f13e4334819b26f79a184e56'
