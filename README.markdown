4a9f61ee5c6b7b41e007e6afd7fc4df36e29e7ab
Hatem Mahmoud <hatem@expressionlab.com>
2010-10-10 02:06:23 +0200

    fixed task scopes and updated readme and css

--- README.markdown
+++ README.markdown
@@ -1,3 +1,14 @@
 # Tasky
 
 Tasky is the best task manager for [Getting Things Done](http://en.wikipedia.org/wiki/Getting_Things_Done).
+
+## Features
+
+- Group related tasks in projects
+- Work in contexts (home, office, phone, etc.)
+- Share projects with others
+- Focus on the next task
+- Track your progress
+- Simple and easy interface
+- Mobile view
+- Free!
--- app/models/task.rb
+++ app/models/task.rb
@@ -4,8 +4,8 @@ class Task < ActiveRecord::Base
   validates_presence_of :name
   
   scope :overdue,  lambda { where("due_at < ? and done = ?", Date.today.midnight, false).order('due_at') }
-  scope :today,    lambda { where(:due_at => Date.today.midnight...Date.tomorrow.midnight).order('due_at') }
-  scope :tomorrow, lambda { where(:due_at => Date.tomorrow.midnight...(Date.tomorrow.midnight + 1.day)).order('due_at') }
-  scope :later,    lambda { where("due_at > ?", Date.tomorrow.midnight + 1.day).order('due_at') }
-  scope :someday,  lambda { where(:due_at => nil).order('name') }
+  scope :today,    lambda { where(:due_at => Date.today.midnight...Date.tomorrow.midnight).order('done, due_at') }
+  scope :tomorrow, lambda { where(:due_at => Date.tomorrow.midnight...(Date.tomorrow.midnight + 1.day)).order('done, due_at') }
+  scope :later,    lambda { where("due_at > ? and done = ?", Date.tomorrow.midnight + 1.day, false).order('due_at') }
+  scope :someday,  lambda { where(:due_at => nil, :done => false).order('name') }
 end
