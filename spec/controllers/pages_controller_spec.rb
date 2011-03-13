ef0412ad0b53ef012bf36989ed6615c7d3d77ef7
Hatem Mahmoud <hatem@expressionlab.com>
2011-03-13 20:37:18 +0200

    fixed pages#home spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,9 +2,9 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe "GET 'show'" do
+  describe "GET 'home'" do
     it "should be successful" do
-      get 'show'
+      get 'show', :id => 'home'
       response.should be_success
     end
   end
