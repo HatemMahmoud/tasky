e274745fd0f44a9cbdbc2afc9b71327ff8f6ba18
Hatem Mahmoud <hatem@expressionlab.com>
2011-03-13 19:27:03 +0200

    rails new tasky -T -J

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tasky::Application.config.secret_token = 'fed955427195bcfb899c937efc2d6482be532427d049538051d2f81337cdbcc634ebc0246632e417bdcb4defdf35e6b5319090aadb60cd5e95e5ddccada900c6'
