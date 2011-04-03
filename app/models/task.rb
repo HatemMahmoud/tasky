00af0d7bee757cfa92329bcf55fbf8da5cb5b9af
Hatem Mahmoud <hatem@expressionlab.com>
2011-04-04 00:37:17 +0200

    fixed task scopes

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,9 +4,9 @@ class Task < ActiveRecord::Base
   
   scope :done, where(:done => true).order('due_at, name')
   scope :due, where(:done => false).order('due_at, name')
-  scope :overdue,  lambda { due.where("due_at < ?", Date.current.midnight) }
-  scope :today,    lambda { due.where(:due_at => Date.current.midnight...Date.tomorrow.midnight) }
-  scope :tomorrow, lambda { due.where(:due_at => Date.tomorrow.midnight...(Date.tomorrow.midnight + 1.day)) }
-  scope :later,    lambda { due.where("due_at > ?", Date.tomorrow.midnight + 1.day) }
+  scope :overdue,  lambda { due.where("due_at < ?", Date.current) }
+  scope :today,    lambda { due.where(:due_at => Date.current...Date.tomorrow) }
+  scope :tomorrow, lambda { due.where(:due_at => Date.tomorrow...(Date.tomorrow + 1.day)) }
+  scope :later,    lambda { due.where("due_at >= ?", Date.tomorrow + 1.day) }
   scope :someday,  lambda { due.where(:due_at => nil) }
 end
