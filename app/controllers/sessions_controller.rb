b9f749cad5a4218556b9dc8fbbfc70cd7303c9d5
Hatem Mahmoud <hatem@expressionlab.com>
2011-05-06 16:53:17 +0200

    login redirect_to tasks_path

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
     auth = request.env["omniauth.auth"]
     user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id  
-    redirect_to request.env['omniauth.origin'] || tasks_path
+    redirect_to tasks_path
   end
   
   def destroy  
