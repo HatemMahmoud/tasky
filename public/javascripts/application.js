8bd876a4feaf61857a666f9a9c2c3e31eaef76f6
Hatem Mahmoud <hatem@expressionlab.com>
2011-05-07 14:41:02 +0200

    fixed .tag layout and style

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,14 @@
 $(document).keypress(function(e) {
-  if (e.keyCode == 27) {  // esc 
+  if (e.keyCode == 27) {  // esc
     $(".editable_form").hide();
-    $(".editable").css("display", "inline"); 
+    $(".editable").css("display", "inline");
+    $(".tag").show(); 
   }
 });
 
 $(".editable").live("click", function() {
   $(this).hide();
+  $(this).siblings(".tag").hide();
   $(this).siblings("form").css("display", "inline");
 });
 
